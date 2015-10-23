fcf2bef1113b9fcb4eb5bfd1429a2797d40c5086
MikhailSkiy <Cold9992006@yandex.ru>
2015-10-24 00:50:18 +0300

    created 1st page and 1 radiobtn

--- ubercalculator/ViewController.swift
+++ ubercalculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newCustomerBtn: UIButton!
+    @IBOutlet weak var existingCustomerBtn: UIButton!
+    
+    var isNewCustomer = false
+    var isExistingCustomer = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func turnOffExistingCustomer(){
+        isExistingCustomer = false
+        let unchecked = UIImage(named:"unchecked")
+        existingCustomerBtn.setImage(unchecked, forState: .Normal)
+    }
+    
+    @IBAction func newCustomerBtnChanged(sender: AnyObject) {
+        if (isNewCustomer){
+            let uncheckedImage = UIImage(named: "unchecked")
+            newCustomerBtn.setImage(uncheckedImage, forState: .Normal)
+            isNewCustomer = false
+        } else {
+            let checked = UIImage(named: "checked")
+            newCustomerBtn.setImage(checked, forState: .Normal)
+            isNewCustomer = true
+        }
+        turnOffExistingCustomer()
+    }
 
 
+    @IBAction func existingCustomerBtnChanged(sender: AnyObject) {
+    }
 }
 
